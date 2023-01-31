e958da19f43505fd352158ad7d3c0de1f112578c
bollo-omar <bolloaggrey@gmail.com>
2023-01-31 06:15:18 -0800

    Task 1 revision

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,13 +1,9 @@
 #!/usr/bin/env ruby
 
-input_str = ARGV[0]
 regex = /(\w)\1{1,4}/
 
-words = input_str.split
-
-words.each do |word|
+ARGV.each do |word|
   if match = word.match(regex)
     puts word
   end
 end
-
